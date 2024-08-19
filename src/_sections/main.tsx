f545a71f8f3425bf317fb732313c048657cca7be
Flibouche <kevin.pfiffer2@gmail.com>
2024-08-19 16:43:18 +0200

    Add all projects to the portfolio.

--- src/_sections/main.tsx
+++ src/_sections/main.tsx
@@ -128,24 +128,126 @@ const MainSkills = () => {
 
 const services = [
     {
-        src: "",
-        title: "",
-        description: "",
-    }
+        src: "icons/laptop-code-solid.svg",
+        alt: "Laptop code",
+        title: "Développement web",
+        description: "Création de sites vitrine et e-commerce, optimisés, esthétiques et accessibles depuis tous les appareils.",
+    },
+    {
+        src: "icons/mobile-screen-button-solid.svg",
+        alt: "Mobile phone",
+        title: "Développement mobile",
+        description: "Améliorez la présence de votre société en créant votre application multi-plateforme, compatible Android / iOS."
+    },
+    {
+        src: "icons/paintbrush-solid.svg",
+        alt: "Paintbrush",
+        title: "Design",
+        description: "Création de logos, cartes de visite, affiches, pancartes, flyers, goodies, marquage automobile, t-shirts..."
+    },
 ]
 
 const MainServices = () => {
     return (
         <div>
-
+            <h2>Nos services</h2>
+            {services.map((service, index) => (
+                <div key={index}>
+                    <img src={service.src} alt={service.alt} width={50} height={50} />
+                    <h3>{service.title}</h3>
+                    <p>{service.description}</p>
+                </div>
+            ))}
         </div>
     )
 }
 
+const projects = [
+    {
+        src: "portfolio/fluriservices.webp",
+        alt: "Fluri Services",
+        title: "Fluri Services - Site web",
+        description: "Fluri Services, après avoir travaillé avec nous pour son activité de taxi, souhaitait mettre en avant son activité de livraison et transport de marchandises."
+    },
+    {
+        src: "portfolio/kaely-greenhouse.webp",
+        alt: "Kaëly Greenhouse",
+        title: "Kaëly Greenhouse - Site web",
+        description : "Lancée dans la vente de produits cosmétiques et bien-être, Kaëly Greenhouse avait besoin d’un site vitrine pour présenter son activité et faciliter le contact avec les clients."
+    },
+    {
+        src: "portfolio/maxime-mettey.webp",
+        alt: "Maxime Mettey",
+        title: "Maxime Mettey - Site web",
+        description: "Le gérant de l'agence CO2M - WebDesign & Communication a réalisé à ses débuts un site web portfolio, afin de présenter ses créations et augmenter sa crédibilité."
+    },
+    {
+        src: "portfolio/white-buchery.webp",
+        alt: "White Buchery",
+        title: "White Buchery - Site web",
+        description: "Le groupe Franc-Comtois White Butchery, de style Death Metal / Deathcore, avait besoin d’un site internet pour partager ses dernières actualités, ses dates de concert et ses photos."
+    },
+    {
+        src: "portfolio/print-cartes-de-visite-kaely-greenhouse.webp",
+        alt: "Cartes de visite Kaëly Greenhouse",
+        title : "Kaëly Greenhouse - Cartes de visite",
+        description: "Lancée dans la vente de produits cosmétiques et bien-être, Kaëly Greenhouse avait besoin de cartes de visites pour faire de la prospection."
+    },
+    {
+        src: "portfolio/print-cartes-de-visite-maxime-mettey.webp",
+        alt: "Cartes de visite Maxime Mettey",
+        title: "Maxime Mettey - Cartes de visite",
+        description: "Avant de lancer l'agence CO2M, je travaillais sous mon propre nom. Découvrez ma toute première carte de visite."
+    },
+    {
+        src: "portfolio/print-cartes-de-visite-kaiden-shop.webp",
+        alt: "Cartes de visite Kaïden Shop",
+        title: "Kaïden Shop - Cartes de visite",
+        description: "En parallèle à leur Site e-commerce, nous avons également réalisé les cartes de visite de Kaïden Shop – Prêt-à-porter féminin et Dépôt-vente femmes & enfants."
+    },
+    {
+        src: "portfolio/print-cartes-de-visite-co2m.webp",
+        alt: "Cartes de visite CO2M",
+        title: "CO2M - Cartes de visite",
+        description: "Dans l'optique de créer une carte de visite à l'image de la société, nous avons regroupé les idées qui nous poussent depuis le lancement de l'agence."
+    },
+    {
+        src: "portfolio/kaidenshop.webp",
+        alt: "Kaïden Shop",
+        title: "Kaïden Shop - Site web e-commerce",
+        description: "Kaïden Shop, ayant récemment lancé son activité de vente de prêt-à-porter féminin et dépôt vente femmes & enfants, avait besoin de se positionner sur Internet pour pouvoir agrandir son périmètre d'activité. C'est pour cela que nous leur avons proposé un site e-commerce !"
+    }
+]
+
 const MainPortfolio = () => {
     return (
         <div>
-
+            <h2>Notre Portfolio</h2>
+            <p>Retrouvez nos dernières réalisations</p>
+            <nav aria-label='Portfolio navigation'>
+                <ul className='flex'>
+                    <li><a href="#">Tout voir</a></li>
+                    <li><a href="#">Développement web</a></li>
+                    <li><a href="#">Print</a></li>
+                </ul>
+            </nav>
+            <div>
+                {projects.map((project, index) => (
+                    <div key={index} className='relative overflow-hidden'>
+                        <img src={project.src} alt={project.alt} width={250} height={250} />
+                        <div className='absolute top-0 left-0 text-black'>
+                            <h3>{project.title}</h3>
+                            <p>{project.description}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className='flex'>
+                <div>
+                <h3>Vous aimez nos projets ?</h3>
+                <span>Contactez-nous, et discutons de votre projet</span>
+                </div>
+            </div>
         </div>
     )
 }
