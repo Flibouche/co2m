595a866300e07998e98bf1daca06fc6c058621f6
Flibouche <kevin.pfiffer2@gmail.com>
2024-08-19 11:55:21 +0200

    Add differents components to the header.

---
+++ src/_components/mobile-menu.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+// import Image from "next/image";
+import { useState } from "react";
+
+export const MobileMenu = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleMenuToggle = () => {
+        setIsOpen(!isOpen);
+    }
+
+    return (
+        <>
+            <div className="md:hidden cursor-pointer" onClick={handleMenuToggle}>
+                <img src="/menu.svg" alt="Menu" width={40} height={40} />
+            </div>
+
+            <div className={`md:hidden fixed top-0 right-0 w-full h-full bg-red-500 z-50 transition-transform duration-300 ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+                <button className="absolute top-4 right-4 text-2xl" onClick={handleMenuToggle}>&times;</button>
+                <nav className="flex flex-col items-center mt-16 space-y-4">
+                    <ul>
+                        <li></li>
+                    </ul>
+                </nav>
+            </div>
+        </>
+    )
+}
--- src/_sections/header.tsx
+++ src/_sections/header.tsx
@@ -1,8 +1,39 @@
+// import Image from "next/image";
+import { MobileMenu } from "../_components/mobile-menu";
+
 const Header = () => {
     return (
-        <header className="bg-red-500">
-            <h1>Header</h1>
+        <header className="w-full min-h-[75px] flex justify-center items-center border-b-2 border-black">
+            <Nav />
         </header>
+    )
+}
+
+const Nav = () => {
+    return (
+        <nav className="w-full max-w-[1120px] flex justify-between items-center px-[10px]">
+            <NavLogo />
+            <NavLinks />
+            <MobileMenu />
+        </nav>
+    )
+}
+
+const NavLogo = () => {
+    return (
+        <div className="flex items-center">
+            <img src="/logo.png" alt="CO2M's logo" width={75} height={75} />
+        </div>
+    )
+}
+
+const NavLinks = () => {
+    return (
+        <div>
+            <ul>
+                <li></li>
+            </ul>
+        </div>
     )
 }
 
