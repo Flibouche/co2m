dd2b3d87a2ed0214d105be9fba562658e57890fb
Flibouche <kevin.pfiffer2@gmail.com>
2024-08-23 11:06:44 +0200

    Add motion.div to the hero section.

--- src/_sections/main.tsx
+++ src/_sections/main.tsx
@@ -136,19 +136,21 @@ const MainHero = () => {
                 <h2 className="text-left font-jost text-2xl md:text-5xl xl:text-7xl pl-2">WEBDESIGN <span className="text-xl md:text-4xl xl:text-5xl">&</span></h2>
                 <h2 className="font-jost text-2xl md:text-5xl xl:text-7xl pl-2">COMMUNICATION</h2>
             </div>
-            <div className="w-[35px] h-[64px] rounded-3xl border-4 border-white flex justify-center items-start p-2">
-                <motion.div
-                    animate={{
-                        y: [0, 24, 0],
-                    }}
-                    transition={{
-                        duration: 1.5,
-                        repeat: Infinity,
-                        repeatType: "loop",
-                    }}
-                    className="w-3 h-3 rounded-full bg-white mb-1"
-                />
-            </div>
+            <ScrollLink to="about" spy={true} smooth={true} duration={500} offset={-70} isDynamic={true} className="cursor-pointer">
+                <div className="absolute bottom-14 w-[35px] h-[64px] rounded-3xl border-4 border-white flex justify-center items-start p-2 opacity-25">
+                    <motion.div
+                        animate={{
+                            y: [0, 24, 0],
+                        }}
+                        transition={{
+                            duration: 1.5,
+                            repeat: Infinity,
+                            repeatType: "loop",
+                        }}
+                        className="w-3 h-3 rounded-full bg-white mb-1"
+                    />
+                </div>
+            </ScrollLink>
         </section>
     )
 }
