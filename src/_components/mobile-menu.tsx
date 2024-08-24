6d5b25b7075f002ad907b2fa0428bd7551ef3efe
Flibouche <kevin.pfiffer2@gmail.com>
2024-08-24 11:45:37 +0200

    Add bg-black to the mobile-menu because backdrop-blur doesn't work fine on mobile.

--- src/_components/mobile-menu.tsx
+++ src/_components/mobile-menu.tsx
@@ -16,7 +16,7 @@ export const MobileMenu = () => {
                 <img src="/menu.svg" alt="Menu" width={40} height={40} className="filter invert brightness-0" />
             </div>
 
-            <div className={`md:hidden fixed top-0 right-0 w-full h-screen backdrop-blur z-20 transition-transform duration-700 ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+            <div className={`md:hidden fixed top-0 right-0 w-full h-screen backdrop-blur bg-black/80 z-20 transition-transform duration-700 ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}>
                 <button className="absolute top-3 right-12 text-6xl" onClick={handleMenuToggle}>&times;</button>
                 <nav className="flex flex-col justify-center items-center h-screen">
                     <ul className="text-4xl text-left font-jost font-medium space-y-2">
